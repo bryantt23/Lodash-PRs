c9998e020e33e8ec47d9377bcecc546775892238
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-02-23 23:50:30 -0800

    Get PR count

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const app = express()
 const port = 3000
 
 app.get("/", (req, res) => {
-    res.send("hello world")
+    res.send("Hello world")
+})
+
+app.get("/pr-count", async (req, res) => {
+    try {
+        const response = await axios.get('https://api.github.com/repos/lodash/lodash/pulls')
+        res.send(`Current lodash pull request count is ${response.data.length}`)
+    } catch (error) {
+        res.status(500).send("Error fetchign pull requests")
+
+    }
 })
 
 app.listen(port, () => {
